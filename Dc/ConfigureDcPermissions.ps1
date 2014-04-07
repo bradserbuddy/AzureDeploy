ef187735e1adc63c50bf47abfe52752873889077
Brad Serbus <brad@buddy.com>
2014-04-06 22:52:52 -0700

    Use variables for domain DC= names.

--- Dc/ConfigureDcPermissions.ps1
+++ Dc/ConfigureDcPermissions.ps1
@@ -4,8 +4,9 @@
     $sid = new-object System.Security.Principal.SecurityIdentifier (Get-ADUser $installUserName).SID
     $guid = new-object Guid bf967a86-0de6-11d0-a285-00aa003049e2
     $ace1 = new-object System.DirectoryServices.ActiveDirectoryAccessRule $sid,"CreateChild","Allow",$guid,"All"
-    $corp = Get-ADObject -Identity "DC=corp,DC=buddy,DC=com"
+    $dcDomainName = "DC=$domainName,DC=$buddyplatformDomainName,DC=com"
+	$corp = Get-ADObject -Identity $dcDomainName
     $acl = Get-Acl $corp
     $acl.AddAccessRule($ace1)
-    Set-Acl -Path "DC=corp,DC=buddy,DC=com" -AclObject $acl
+    Set-Acl -Path $dcDomainName -AclObject $acl
 }

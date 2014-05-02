e6ac2810f0d7f40403a41b2c2d9cb1603aed6631
Brad Serbus <brad@buddy.com>
2014-05-02 15:55:51 -0700

    Add SSH firewall rules to web & queue boxes.

--- Common/SetSshEndpoints.ps1
+++ Common/SetSshEndpoints.ps1
@@ -8,8 +8,22 @@
             Update-AzureVM
 }
 
+function EnableSshFirewall($serverName)
+{
+    $enableSshScriptBlock =
+    {
+        New-NetFirewallRule -Name "SSH" -DisplayName "SSH" -LocalPort 22 -Protocol TCP
+    }
+    
+    RunRemotely $vmAdminUser $vmAdminPassword $dcCloudServiceName $serverName $enableSshScriptBlock
+}
+
 SetSshEndpoints $webServerName1   20022 
 SetSshEndpoints $webServerName2   20122
 SetSshEndpoints $dcServerName     21022
 SetSshEndpoints $mongoServerName1 22022
 SetSshEndpoints $mongoServerName2 22122
+
+EnableSshFirewall $webServerName1
+EnableSshFirewall $webServerName2
+EnableSshFirewall $dcServerName
